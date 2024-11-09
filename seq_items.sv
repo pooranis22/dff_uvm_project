d1d7137c0d5ecc0644a9bea54f8a839b7ca71443
Poorani Sinouvassane <71457620+pooranis22@users.noreply.github.com>
2024-11-09 16:48:42 -0600

    Update seq_items.sv

--- seq_items.sv
+++ seq_items.sv
@@ -1,7 +1,9 @@
 
 class dff_seq_item extends uvm_seq_item;
   `uvm_object_utilis(dff_seq_item) //register class to factory
-  
+  rand logic reset;
+  rand logic d;
+  logic q;
   //standard constructor
   function new(string name = "dff_seq_item");
     super.new(name);
